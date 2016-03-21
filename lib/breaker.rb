d84be07c965cd11a385434d545c92c9d66df1130
Garrett Thornburg <film42@gmail.com>
2016-03-20 23:51:05 -0600

    Base gem stuff

---
+++ lib/breaker.rb
@@ -0,0 +1,8 @@
+require "breaker/version"
+
+require "breaker/config"
+require "breaker/sampler/sliding_window_sampler"
+
+module Breaker
+  # Your code goes here...
+end
---
+++ spec/breaker_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Breaker do
+end
